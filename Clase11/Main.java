a01a8e62a47fa1f7992736ac74f98ff78df77ae8
JuanDavid-6 <juand.bolanos@campusucc.edu.co>
2026-04-29 00:53:01 +0000

    Clase Martes 28

--- Clase11/Main.java
+++ Clase11/Main.java
@@ -1,23 +1,52 @@
 package Clase11;
 
 public class Main {
-    
-public static void main(String[] args) {
 
-        Orden orden = new Orden("12:00", "Pedro");
+    public static void main(String[] args) {
 
-        orden.agregarBebida(new Bebida(2, "Gaseosa"));
-        orden.agregarPlatillo(new Platillo(1, "Pizza"));
+        Cliente cliente = new Cliente(5, 3);
+        System.out.println("Mesa: " + cliente.getMesa());
+        System.out.println("Comensales: " + cliente.getComensales());
 
-        double subTotal = 30000;
-        double propina = 3000;
-        double impuestos = 2400;
+        Orden orden = new Orden("1:00 pm", "Carlos");
+        System.out.println("\nMesero: Carlos");
+        System.out.println("Hora: 1:00 pm");
 
-        Efectivo pago = new Efectivo(subTotal, propina, impuestos, 40000);
+        Bebida b1 = new Bebida(2, "Gaseosa");
+        Bebida b2 = new Bebida(1, "Jugo");
+
+        orden.agregarBebida(b1);
+        orden.agregarBebida(b2);
+
+        Platillo p1 = new Platillo(1, "Hamburguesa");
+        Platillo p2 = new Platillo(2, "Pizza");
+
+        orden.agregarPlatillo(p1);
+        orden.agregarPlatillo(p2);
+
+        System.out.println("\n--- PEDIDO ---");
+
+        System.out.println("Bebidas:");
+        System.out.println("- " + b1.getTipo() + " x" + b1.getCantidad());
+        System.out.println("- " + b2.getTipo() + " x" + b2.getCantidad());
+
+        System.out.println("\nPlatillos:");
+        System.out.println("- " + p1.getTipo() + " x" + p1.getCantidad());
+        System.out.println("- " + p2.getTipo() + " x" + p2.getCantidad());
+
+        double subTotal = 50000;
+        double propina = 5000;
+        double impuestos = 4000;
+
+        Efectivo pago = new Efectivo(subTotal, propina, impuestos, 70000);
 
         double total = pago.calculoTotal(subTotal, propina, impuestos);
 
-        System.out.println("Total: " + total);
+        System.out.println("\n--- FACTURA ---");
+        System.out.println("Subtotal: " + subTotal);
+        System.out.println("Propina: " + propina);
+        System.out.println("Impuestos: " + impuestos);
+        System.out.println("Total a pagar: " + total);
 
         pago.pagar();
     }
